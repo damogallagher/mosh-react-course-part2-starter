774c82e07446c8c99103029c74b6db333e44a6c3
Damien Gallagher <damien.gallagher@gmail.com>
2024-04-02 22:20:02 +0100

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, //5 minutes
+      staleTime: 10_000, //10 seconds,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+      refetchOnMount: true,
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
